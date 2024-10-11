23886cd9b90b071b430a2e93aa9e64442810fdd0
Md Atikuzzaman <akash203037@gmail.com>
2024-10-11 22:09:17 +0600

    form action added

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -1,11 +1,20 @@
+"use client"
 import React from 'react';
 
 const GetInTouch = () => {
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    const form = e.target
+    const businessName = form.businessName.value
+    const email = form.email.value
+    const number = form.phone.value
+    alert(businessName, email, number)
+  }
   return (
     <section className="mt-24  p-24 rounded-md max-w-screen-xl mx-auto" id="contacts">
       <h2 className="font-semibold text-blue-400 text-4xl text-center">Get In Touch</h2>
       <div className="mt-8">
-        <form action="" className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
           <input className="w-full text-center bg-transparent outline-violet-950 border border-blue-950 p-2 rounded-md" type="text" name="businessName" id="businessName" placeholder="Business Name"/>
           <input className="w-full text-center bg-transparent outline-violet-950 border border-blue-950 p-2 rounded-md" type="email" name="email" id="email" placeholder="Email"/>
            <input className="w-full text-center bg-transparent outline-violet-950 border border-blue-950 p-2 rounded-md" type="tel" name="phone" id="phone" placeholder="Phone Number" />
